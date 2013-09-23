6b3af9ca9cbf3eb46726d03cf859dfa782626b03
Sean Williams <unixed@gmail.com>
2013-09-23 13:06:24 -0700

    Changed termination protocol; changed options syntax; cleaned up some stuff

--- src/icmp_server.c
+++ src/icmp_server.c
@@ -32,162 +32,178 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <netinet/ip_icmp.h>
 #include <netinet/ether.h>
 
-unsigned short checksum (unsigned short *addr, int len)
-{
-   int nleft = len;
-   int sum = 0;
-   unsigned short *w = addr;
-   unsigned short answer = 0;
-   while (nleft > 1){
-      sum += *w++;
-      nleft -= 2;
-   }
-   if (nleft == 1){
-      *(unsigned char *) (&answer) = *(unsigned char *) w;
-      sum += answer;
-   }
-   sum = (sum >> 16) + (sum & 0xffff);
-   sum += (sum >> 16);
-   answer = ~sum;
-   return (answer);
+#define OPT_HELP 1
+#define OPT_EXEC_MODE 2
+#define OPT_FILENAME 4
+#define OPT_RESPOND_TO_CLIENT 8
+#define OPT_OUTPUT_HASH 16
+
+struct options {
+  unsigned long long flags;
+} options;
+
+unsigned short checksum (unsigned short *addr, int len) {
+  int nleft = len;
+  int sum = 0;
+  unsigned short *w = addr;
+  unsigned short answer = 0;
+  while (nleft > 1) {
+    sum += *w++;
+    nleft -= 2;
+  }
+  if (nleft == 1) {
+    *(unsigned char *) (&answer) = *(unsigned char *) w;
+    sum += answer;
+  }
+  sum = (sum >> 16) + (sum & 0xffff);
+  sum += (sum >> 16);
+  answer = ~sum;
+  return (answer);
 }
 
-void usage(){
-   fprintf(stderr, "Usage: ./bin [-f <filename>]\n");
-   exit(EXIT_SUCCESS);
+void usage() {
+  fprintf(stderr, "Usage: ./bin [-f <filename>]\n");
+  exit(EXIT_SUCCESS);
 }
 
-int main (int argc, char **argv)
-{
-   int i = 0;
-   int ret = 0;
-   int one = 1;
-   int sock_eth;
-   int sock_icmp;
-   int fd;
-   int ip_len;
-   int icmp_len;
-   int icmp_data_in_len;
-   int opt = 0;
-   char print_hash = 1;
-   char *filename = NULL;
-   unsigned char sha1hash[20];
-   char buf_incoming[1500];
-   char buf_outgoing[1500];
-   char payload[50];
-   char exec_mode = 0;
-   struct sockaddr_in dst;
-   struct ether_header *eth_hdr;
-   struct ip *ip_hdr_in, *ip_hdr_out;
-   struct icmp *icmp_hdr_in, *icmp_hdr_out;
+int main (int argc, char **argv) {
+  int i = 0;
+  int ret = 0;
+  int one = 1;
+  int sock_eth;
+  int sock_icmp;
+  int fd;
+  int ip_len;
+  int icmp_len;
+  int icmp_data_in_len;
+  int opt = 0;
+  char *filename = NULL;
+  unsigned char sha1hash[20];
+  char buf_incoming[1500];
+  char buf_outgoing[1500];
+  char payload[50];
+  char exec_mode = 0;
+  struct options opts;
+  struct sockaddr_in dst;
+  struct ether_header *eth_hdr;
+  struct ip *ip_hdr_in, *ip_hdr_out;
+  struct icmp *icmp_hdr_in, *icmp_hdr_out;
 
-   while ((opt = getopt(argc, argv, "hncf:")) != -1) {
-      switch (opt) {
-      case 'f':
-         filename = optarg;
-         break;
-      case 'c':
-         exec_mode = 1;
-         break;
-      case 'n':
-         print_hash = 0;
-         break;
-      case 'h':
-      default:
-         usage();
-      }
+  memset(&opts, 0, sizeof(options)); 
+  while ((opt = getopt(argc, argv, "hHcf:")) != -1) {
+    switch (opt) {
+    case 'f':
+      filename = optarg;
+      opts.flags |= OPT_FILENAME;
+      break;
+    case 'c':
+      exec_mode = 1;
+      opts.flags |= OPT_EXEC_MODE;
+      break;
+    case 'r':
+      opts.flags |= OPT_RESPOND_TO_CLIENT;
+      break;
+    case 'H':
+      opts.flags |= OPT_OUTPUT_HASH;
+      break;
+    case 'h':
+    default:
+      usage();
+    }
+  }
+
+  if ((sock_eth = socket (AF_INET, SOCK_PACKET, htons (ETH_P_ALL))) < 0) {
+    perror ("socket");
+    exit (1);
    }
-
-   if ((sock_eth = socket (AF_INET, SOCK_PACKET, htons (ETH_P_ALL))) < 0)
-   {
-      perror ("socket");
-      exit (1);
-    }
-  if ((sock_icmp = socket (AF_INET, SOCK_RAW, IPPROTO_ICMP)) < 0)
-    {
-      perror ("socket");
-      exit (1);
-    }
+  if ((sock_icmp = socket (AF_INET, SOCK_RAW, IPPROTO_ICMP)) < 0) {
+    perror ("socket");
+    exit (1);
+   }
   if ((ret =
-       setsockopt (sock_icmp, IPPROTO_IP, IP_HDRINCL, (char *) &one,
-		   sizeof (one))) < 0)
-    {
-      perror ("setsockopt");
-      exit (1);
+     setsockopt (sock_icmp, IPPROTO_IP, IP_HDRINCL, (char *) &one,
+		  sizeof (one))) < 0) {
+    perror ("setsockopt");
+    exit (1);
+  }
+
+  if (opts.flags & OPT_FILENAME && filename) {
+    if ((fd = open(filename, O_WRONLY|O_CREAT|O_TRUNC|O_SYNC, S_IRUSR)) == -1) {
+      perror("open");
+      exit(1);
     }
+  } else {
+    fd = STDOUT_FILENO;
+  }
 
-   if (filename){
-      if ((fd = open(filename, O_WRONLY|O_CREAT|O_TRUNC|O_SYNC, S_IRUSR)) == -1){
-         perror("open");
-         exit(1);
+  eth_hdr = (struct ether_header *) buf_incoming;
+
+  ip_hdr_in = (struct ip *) (buf_incoming + sizeof (struct ether_header));
+  icmp_hdr_in = (struct icmp *) ((unsigned char *) ip_hdr_in + sizeof (struct ip));
+
+  ip_hdr_out = (struct ip *) buf_outgoing;
+  icmp_hdr_out = (struct icmp *) (buf_outgoing + sizeof (struct ip));
+
+  while ((ret = recv (sock_eth, buf_incoming, sizeof (buf_incoming), 0)) > 0) {
+    if (ip_hdr_in->ip_p == IPPROTO_ICMP) {
+       icmp_hdr_out->icmp_type = ICMP_ECHOREPLY;
+       icmp_hdr_out->icmp_code = 0;
+      if (icmp_hdr_in->icmp_type == ICMP_ECHO) {
+        ip_hdr_out->ip_v = ip_hdr_in->ip_v;
+        ip_hdr_out->ip_hl = ip_hdr_in->ip_hl;
+        ip_hdr_out->ip_tos = ip_hdr_in->ip_tos;
+        ip_hdr_out->ip_len = ip_hdr_in->ip_len;
+        ip_hdr_out->ip_id = ip_hdr_in->ip_id;
+        ip_hdr_out->ip_off = 0;
+        ip_hdr_out->ip_ttl = 255;
+        ip_hdr_out->ip_p = IPPROTO_ICMP;
+        ip_hdr_out->ip_sum = 0;
+        ip_hdr_out->ip_src.s_addr = ip_hdr_in->ip_dst.s_addr;
+        ip_hdr_out->ip_dst.s_addr = ip_hdr_in->ip_src.s_addr;
+        ip_hdr_out->ip_sum = checksum ((unsigned short *) buf_outgoing, ip_hdr_out->ip_hl);
+
+        icmp_hdr_out->icmp_type = ICMP_ECHOREPLY;
+        icmp_hdr_out->icmp_code = 0;
+        icmp_hdr_out->icmp_id = icmp_hdr_in->icmp_id;
+        icmp_hdr_out->icmp_seq = icmp_hdr_in->icmp_seq + 1;
+        icmp_hdr_out->icmp_cksum = 0;
+
+        ip_len = ntohs (ip_hdr_in->ip_len);
+        icmp_len = ip_len - sizeof (struct iphdr);
+        icmp_data_in_len = ntohs(ip_hdr_in->ip_len) - sizeof(struct ip) - sizeof(struct icmphdr);
+        
+        memset(payload, 0, sizeof(payload));
+        memcpy(payload, icmp_hdr_in->icmp_data, icmp_data_in_len);
+        // TODO: change this crappy protocol that determines conversation termination this way
+        if (payload[0] == '\0' && icmp_data_in_len == 0) {
+          break;
+        }
+        write(fd, payload, icmp_data_in_len);
+
+        if (opts.flags & OPT_RESPOND_TO_CLIENT) {
+          memcpy(icmp_hdr_out->icmp_data, payload, icmp_data_in_len);
+          dst.sin_family = AF_INET;
+          dst.sin_addr.s_addr = ip_hdr_out->ip_dst.s_addr;
+          icmp_hdr_out->icmp_cksum = checksum ((unsigned short *) icmp_hdr_out, icmp_len);
+          ret = sendto(sock_icmp, buf_outgoing, ip_len, 0, (struct sockaddr *) &dst, sizeof (dst));
+          if (ret < 0) {
+            perror ("sendto");
+            exit(1);
+          }
+        }
       }
-   }else{
-      fd = STDOUT_FILENO;
-   }
-
-   eth_hdr = (struct ether_header *) buf_incoming;
-
-   ip_hdr_in = (struct ip *) (buf_incoming + sizeof (struct ether_header));
-   icmp_hdr_in = (struct icmp *) ((unsigned char *) ip_hdr_in + sizeof (struct ip));
-
-   ip_hdr_out = (struct ip *) buf_outgoing;
-   icmp_hdr_out = (struct icmp *) (buf_outgoing + sizeof (struct ip));
-
-   while ((ret = recv (sock_eth, buf_incoming, sizeof (buf_incoming), 0)) > 0){
-      if (ip_hdr_in->ip_p == IPPROTO_ICMP){
-         if (icmp_hdr_in->icmp_type == ICMP_ECHO){
-            ip_hdr_out->ip_v = ip_hdr_in->ip_v;
-            ip_hdr_out->ip_hl = ip_hdr_in->ip_hl;
-            ip_hdr_out->ip_tos = ip_hdr_in->ip_tos;
-            ip_hdr_out->ip_len = ip_hdr_in->ip_len;
-            ip_hdr_out->ip_id = ip_hdr_in->ip_id;
-            ip_hdr_out->ip_off = 0;
-            ip_hdr_out->ip_ttl = 255;
-            ip_hdr_out->ip_p = IPPROTO_ICMP;
-            ip_hdr_out->ip_sum = 0;
-            ip_hdr_out->ip_src.s_addr = ip_hdr_in->ip_dst.s_addr;
-            ip_hdr_out->ip_dst.s_addr = ip_hdr_in->ip_src.s_addr;
-            ip_hdr_out->ip_sum = checksum ((unsigned short *) buf_outgoing, ip_hdr_out->ip_hl);
-
-            icmp_hdr_out->icmp_type = ICMP_ECHOREPLY;
-            icmp_hdr_out->icmp_code = 0;
-            icmp_hdr_out->icmp_id = icmp_hdr_in->icmp_id;
-            icmp_hdr_out->icmp_seq = icmp_hdr_in->icmp_seq + 1;
-            icmp_hdr_out->icmp_cksum = 0;
-
-            ip_len = ntohs (ip_hdr_in->ip_len);
-            icmp_len = ip_len - sizeof (struct iphdr);
-            icmp_data_in_len = ntohs(ip_hdr_in->ip_len) - sizeof(struct ip) - sizeof(struct icmphdr);
-            
-            memset(payload, 0, sizeof(payload));
-            memcpy(payload, icmp_hdr_in->icmp_data, icmp_data_in_len);
-            if (payload[0] == '.' && icmp_data_in_len == 1){
-               break;
-            }
-            write(fd, payload, icmp_data_in_len);
-
-            memcpy(icmp_hdr_out->icmp_data, payload, icmp_data_in_len);
-            dst.sin_family = AF_INET;
-            dst.sin_addr.s_addr = ip_hdr_out->ip_dst.s_addr;
-            icmp_hdr_out->icmp_cksum = checksum ((unsigned short *) icmp_hdr_out, icmp_len);
-            ret = sendto(sock_icmp, buf_outgoing, ip_len, 0, (struct sockaddr *) &dst, sizeof (dst));
-            if (ret < 0){
-               perror ("sendto");
-               exit(1);
-            }
-         }
-      }
-   }
-   memset(sha1hash, 0, sizeof(sha1hash));
-   ret = recv(sock_eth, buf_incoming, sizeof(buf_incoming), 0);
-   memcpy(sha1hash, icmp_hdr_in->icmp_data, sizeof(sha1hash));
-   if (print_hash){
-      for (; i < sizeof(sha1hash); i++)
-         fprintf(stderr, "%02x", sha1hash[i]);
-      fprintf(stderr, "\n");
-   }
-   close(fd);
-   close(sock_eth);
-   close(sock_icmp);
-   return 0;
+    }
+  }
+  memset(sha1hash, 0, sizeof(sha1hash));
+  ret = recv(sock_eth, buf_incoming, sizeof(buf_incoming), 0);
+  memcpy(sha1hash, icmp_hdr_in->icmp_data, sizeof(sha1hash));
+  if (opts.flags & OPT_OUTPUT_HASH) {
+    for (; i < sizeof(sha1hash); i++)
+      fprintf(stderr, "%02x", sha1hash[i]);
+    fprintf(stderr, "\n");
+  }
+  close(fd);
+  close(sock_eth);
+  close(sock_icmp);
+  return 0;
 }
